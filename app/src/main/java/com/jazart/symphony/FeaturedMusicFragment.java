de79d78b91668808b3716acf73606faeb7c2254b
kendrickgholston <kengholston8@gmail.com>
2018-04-16 23:53:50 -0400

    Edited the "Upload" fab to have an upload icon in that button. Added the functionality to get access the file manager and obtain the URI of an mp3 file

--- app/src/main/java/com/jazart/symphony/FeaturedMusicFragment.java
+++ app/src/main/java/com/jazart/symphony/FeaturedMusicFragment.java
@@ -1,19 +1,28 @@
 package com.jazart.symphony;
 
+import android.content.Intent;
+import android.media.MediaMetadataRetriever;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
+import android.app.Activity;
+import android.net.Uri;
+
+import com.github.clans.fab.FloatingActionButton;
 
 public class FeaturedMusicFragment extends Fragment {
     private MusicAdapter mMusicAdapter;
+    private FloatingActionButton upFAB;
+    private Uri uriSound;
 
 
     public FeaturedMusicFragment() {
@@ -23,7 +32,12 @@ public class FeaturedMusicFragment extends Fragment {
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+
     }
+
+
+
 
     @Nullable
     @Override
@@ -32,9 +46,41 @@ public class FeaturedMusicFragment extends Fragment {
         RecyclerView recyclerView = v.findViewById(R.id.featured_songs);
         recyclerView.setAdapter(mMusicAdapter);
         recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        upFAB =  v.findViewById(R.id.fab_upload);
+        upFAB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent musicIntent = new Intent();
+                musicIntent.setAction(Intent.ACTION_GET_CONTENT);
+                musicIntent.setType("audio/mp3");
+                startActivityForResult(Intent.createChooser(
+                        musicIntent, "Open Audio (mp3) file"),1);
+
+
+            }
+        });
 
         return v;
     }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        if (resultCode == Activity.RESULT_OK){
+            //Log.d("DEBUG","RESULT OK");
+            if (requestCode == 1){
+                //Log.d("DEBUG","REQUEST OK");
+                uriSound = data.getData();
+                //Log.d("DEBUG",uriSound.toString());
+                MediaMetadataRetriever musicInfoRetr = new MediaMetadataRetriever();
+                musicInfoRetr.setDataSource(uriSound.toString());
+
+            }
+        }
+    }
+
+
+
 
     @Override
     public void onPause() {
