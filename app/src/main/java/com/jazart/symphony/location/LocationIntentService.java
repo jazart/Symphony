ec6b02cc22eeb1006712790787957cfea31d4988
jazart <payne_jeremy@columbusstate.edu>
2018-05-01 01:01:19 -0400

    -added location storing

--- app/src/main/java/com/jazart/symphony/location/LocationIntentService.java
+++ app/src/main/java/com/jazart/symphony/location/LocationIntentService.java
@@ -4,23 +4,32 @@ import android.Manifest;
 import android.app.IntentService;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
 import android.location.Location;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.ActivityCompat;
+import android.util.Log;
 import android.widget.Toast;
 
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.GeoPoint;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
 
 import static com.jazart.symphony.Constants.USERS;
 import static com.jazart.symphony.MainActivity.EXTRA_USER;
 import static com.jazart.symphony.MainActivity.sDb;
 
 public class LocationIntentService extends IntentService {
+    public static final String TAG = "LocationIntentService";
     FusedLocationProviderClient mProviderClient;
 
     public LocationIntentService() {
@@ -57,14 +66,17 @@ public class LocationIntentService extends IntentService {
         mProviderClient.getLastLocation().addOnSuccessListener(new OnSuccessListener<Location>() {
             @Override
             public void onSuccess(Location location) {
-                addToDb(location, userId);
+                String currentCity = getCityFromLocation(location);
+                addToDb(location, currentCity, userId);
+
             }
         });
     }
 
 
-    private void addToDb(Location location, String userId) {
-        sDb.collection(USERS).document(userId)
+    private void addToDb(Location location, String city, String userId) {
+        DocumentReference reference = sDb.collection(USERS).document(userId);
+        reference
                 .update("location", new GeoPoint(location.getLatitude(), location.getLatitude()))
                 .addOnFailureListener(new OnFailureListener() {
                     @Override
@@ -73,6 +85,29 @@ public class LocationIntentService extends IntentService {
                                 .show();
                     }
                 });
+        reference.update("city", city)
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        //error;
+                    }
+                });
+    }
+
+    private String getCityFromLocation(Location location) {
+        Geocoder geocoder = new Geocoder(this, Locale.getDefault());
+        List<Address> addresses = null;
+        try {
+            addresses = geocoder.getFromLocation(location.getLatitude(), location.getLongitude(), 1);
+        } catch (IOException e) {
+            Log.e(TAG, e.getMessage());
+        }
+
+        if (addresses == null || addresses.size() > 1) {
+            return null;
+        }
+
+        return addresses.get(0).getLocality();
     }
 
 }
