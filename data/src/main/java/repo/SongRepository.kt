924dddad382fa2be28f000688ce067544b6897e6
jazart <payne_jeremy@columbusstate.edu>
2019-10-06 18:30:52 -0700

    General refactoring

--- data/src/main/java/repo/SongRepository.kt
+++ data/src/main/java/repo/SongRepository.kt
@@ -1,9 +1,10 @@
 package com.jazart.data.repo
 
 import entities.Song
+import java.net.URI
 
 interface SongRepository {
-
     suspend fun findSongById(id: String): Song?
     suspend fun findSongsByUserId(id: String): List<Song>
+    suspend fun addSong(song: Song, uri: URI)
 }
